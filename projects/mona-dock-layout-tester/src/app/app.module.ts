2ad1d1f00228fcde1cdfbb827cd922bfc5dfcdcc
Nanahoshi <nanahoshii@outlook.com>
2023-09-13 22:23:37 +0300

    feat: update angular to v16.2

--- projects/mona-dock-layout-tester/src/app/app.module.ts
+++ projects/mona-dock-layout-tester/src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {
+    ButtonDirective,
+    GridColumnComponent,
+    GridComponent,
+    TabComponent,
+    TabStripComponent,
+    TextAreaDirective
+} from "@mirei/mona-ui";
+import {DockLayoutModule} from "mona-dock-layout";
 
-import { AppComponent } from "./app.component";
-import { DockLayoutModule } from "mona-dock-layout";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { ButtonModule, GridModule, TabStripModule, TextAreaModule } from "@mirei/mona-ui";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {AppComponent} from "./app.component";
 
 @NgModule({
     declarations: [AppComponent],
@@ -14,12 +21,15 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
         BrowserAnimationsModule,
         DockLayoutModule,
         FontAwesomeModule,
-        ButtonModule,
-        GridModule,
-        TabStripModule,
-        TextAreaModule
+        ButtonDirective,
+        GridComponent,
+        GridColumnComponent,
+        TabStripComponent,
+        TabComponent,
+        TextAreaDirective
     ],
     providers: [],
     bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- projects/mona-dock-layout/src/lib/dock-layout.module.ts
+++ projects/mona-dock-layout/src/lib/dock-layout.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from "@angular/core";
-import { PanelTemplateReferenceDirective } from "./directives/panel-template-reference.directive";
-import { PanelContentAnchorDirective } from "./directives/panel-content-anchor.directive";
-import { PanelContentTemplateDirective } from "./directives/panel-content-template.directive";
-import { LayoutContentTemplateDirective } from "./directives/layout-content-template.directive";
-import { PanelTitleTemplateDirective } from "./directives/panel-title-template.directive";
-import { PanelActionTemplateDirective } from "./directives/panel-action-template.directive";
-import { CommonModule } from "@angular/common";
 import { DragDropModule } from "@angular/cdk/drag-drop";
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { PanelPositionSelectorPipe } from "./pipes/panel-position-selector.pipe";
-import { PanelComponent } from "./components/panel/panel.component";
-import { ContextMenuModule } from "@mirei/mona-ui";
-import { PanelGroupComponent } from "./components/panel-group/panel-group.component";
-import { DockPanelComponent } from "./components/dock-panel/dock-panel.component";
+import { ContextMenuComponent, MenuItemComponent, MenuItemTextTemplateDirective } from "@mirei/mona-ui";
 import { ContainerComponent } from "./components/container/container.component";
 import { DockLayoutComponent } from "./components/dock-layout/dock-layout.component";
+import { DockPanelComponent } from "./components/dock-panel/dock-panel.component";
+import { PanelGroupComponent } from "./components/panel-group/panel-group.component";
+import { PanelComponent } from "./components/panel/panel.component";
+import { LayoutContentTemplateDirective } from "./directives/layout-content-template.directive";
+import { PanelActionTemplateDirective } from "./directives/panel-action-template.directive";
+import { PanelContentAnchorDirective } from "./directives/panel-content-anchor.directive";
+import { PanelContentTemplateDirective } from "./directives/panel-content-template.directive";
+import { PanelTemplateReferenceDirective } from "./directives/panel-template-reference.directive";
+import { PanelTitleTemplateDirective } from "./directives/panel-title-template.directive";
+import { PanelPositionSelectorPipe } from "./pipes/panel-position-selector.pipe";
 
 @NgModule({
     declarations: [
@@ -31,7 +31,14 @@ import { DockLayoutComponent } from "./components/dock-layout/dock-layout.compon
         ContainerComponent,
         DockLayoutComponent
     ],
-    imports: [CommonModule, DragDropModule, FontAwesomeModule, ContextMenuModule],
+    imports: [
+        CommonModule,
+        DragDropModule,
+        FontAwesomeModule,
+        ContextMenuComponent,
+        MenuItemComponent,
+        MenuItemTextTemplateDirective
+    ],
     exports: [
         DockLayoutComponent,
         DockPanelComponent,
